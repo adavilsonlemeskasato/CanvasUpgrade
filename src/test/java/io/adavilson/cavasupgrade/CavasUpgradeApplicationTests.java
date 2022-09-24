c13d1943548b95144aa0917b6a0a905e72e515a7
Onias da Rocha <onias.filho@gmail.com>
2022-09-24 12:09:29 -0400

    inicializa projeto spring

---
+++ src/test/java/io/adavilson/cavasupgrade/CavasUpgradeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.adavilson.cavasupgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CavasUpgradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
